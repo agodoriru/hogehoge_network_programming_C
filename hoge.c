322d24a05fd0502d27465ffa71ceb1367b776d7e
agodoriru <ag0d0r1rv@gmail.com>
2019-02-07 18:20:06 +0900

    [CLEAN]

--- hoge.c
+++ hoge.c
@@ -35,64 +35,47 @@ int init_descriptor(char *network_interface_name,int promiscuous_flag);
 
 int main(int argc,char *argv[]){
     int soc,size;
-    u_char buff[8191];
+    u_char buff[4096];
 
     //no command line argument
-    if(argc<=1){
+    if(argc <= 1){
         usage(stdout);
         return 1;
     }
 
-    printf("no command line zone clear!\n");
+    printf("%s\n", argv[1]);
 
-    printf("%s\n",argv[1]);
-
-    if(strcmp(argv[1],"show")==0){
+    if(strcmp(argv[1], "show")==0){
         printf("activate interface_checker\n");
         interface_checker();
         return 1;
     }
 
-    if((soc=init_descriptor(argv[1],0))==-1){
-        fprintf(stderr,"init_descriptor:error:%s\n",argv[1]);
+    if((soc=init_descriptor(argv[1], 0)) == -1) {
+        fprintf(stderr, "init_descriptor:error:%s\n", argv[1]);
         return -1;
     }
 
-    printf("initiating descriptor success!\n");
     printf("in while soc:%d\n",soc );
     printf("size:%ld\n", read(soc,buff,sizeof(buff)));
-    sleep(10);
-
 
     while(1){
 
-        if((size=read(soc,buff,sizeof(buff)))<=0){
+        if((size = read(soc, buff, sizeof(buff))) <= 0) {
             perror("read");
-        }
-        else{
-
-
-            if(size>=sizeof(struct ether_header)){
-
-                printf("doing analyze\n");
+        } else {
+            if(size >= sizeof(struct ether_header)) {
                 printf("=====================================================\n");
-
                 get_time(stdout);
                 printf("------------------------------------------------\n");
-                print_EtherHeader((struct ether_header *)buff,stdout);
+                print_EtherHeader((struct ether_header *)buff, stdout);
                 printf("------------------------------------------------\n\n\n");
-                analyze_Packet(buff,size);
+                analyze_Packet(buff, size);
                 printf("--------------------------------------------------\n");
-
-//                sleep(1);
-
-                
                 printf("done analyze\n\n");
-
                 printf("=====================================================\n");
-
-            }else{
-                fprintf(stderr,"read size(%d) < %ld\n", size, sizeof(struct ether_header));
+            } else {
+                fprintf(stderr, "read size(%d) < %ld\n", size, sizeof(struct ether_header));
             }
         }
     }
@@ -107,51 +90,46 @@ int init_descriptor(char *network_interface_name,int promiscuous_flag){
 
     struct ifreq ifreq;
     struct sockaddr_ll sll;
-    int	soc;
+    int soc;
 
-    soc=socket(PF_PACKET,SOCK_RAW,htons(ETH_P_ALL));
+    soc = socket(PF_PACKET, SOCK_RAW, htons(ETH_P_ALL));
 
-    memset(&ifreq,0,sizeof(struct ifreq));
-    strncpy(ifreq.ifr_name,network_interface_name,sizeof(ifreq.ifr_name)-1);
+    memset(&ifreq, 0, sizeof(struct ifreq));
+    strncpy(ifreq.ifr_name,network_interface_name, sizeof(ifreq.ifr_name)-1);
 
     int interface_index=ioctl(soc,SIOCGIFINDEX,&ifreq);
     //if error in initiating socket
 
-    if(interface_index<0){
+    if(interface_index < 0) {
         perror("ioctl");
         close(soc);
         return (-1);
     }
 
-    sll.sll_family=PF_PACKET;
-    sll.sll_protocol=htons(ETH_P_ALL);
-    sll.sll_ifindex=ifreq.ifr_ifindex;
+    sll.sll_family = PF_PACKET;
+    sll.sll_protocol = htons(ETH_P_ALL);
+    sll.sll_ifindex = ifreq.ifr_ifindex;
 
-    if(bind(soc,(struct sockaddr *)&sll,sizeof(sll))<0){
+    if(bind(soc, (struct sockaddr *)&sll, sizeof(sll)) < 0) {
         perror("bind");
         close(soc);
         return (-1);
     }
 
-    if(promiscuous_flag){
-        if(ioctl(soc,SIOCGIFFLAGS,&ifreq)<0){
+    if(promiscuous_flag) {
+        if(ioctl(soc, SIOCGIFFLAGS, &ifreq) < 0) {
             perror("ioctl");
             close(soc);
             return (-1);
         }
 
-        ifreq.ifr_flags=ifreq.ifr_flags|IFF_PROMISC;
+        ifreq.ifr_flags = ifreq.ifr_flags|IFF_PROMISC;
 
-        if(ioctl(soc,SIOCSIFFLAGS,&ifreq)<0){
+        if(ioctl(soc, SIOCSIFFLAGS, &ifreq) < 0) {
             perror("ioctl");
             close(soc);
             return (-1);
         }
     }
-
-    printf("bind to interface clear!\n");
-
-    printf("returned in init_descriptor soc:%u\n",soc );
-
     return(soc);
 }
